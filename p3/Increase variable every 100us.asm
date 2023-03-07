0b9d8d7fc08e85b90c9796ce9efe2f68e5af0979
Álvaro Hernández <103251919+varo6@users.noreply.github.com>
2023-03-07 19:41:52 +0100

    Update timer0 uses

--- p3/Increase variable every 100us.asm
+++ p3/Increase variable every 100us.asm
@@ -1,5 +1,5 @@
 	;Exercise 3.3.  Write  a  program  using  the  Timer  0  module  and  its  interrupt  on  overflow  to  increase  variable 
-	;CICLO_100us every 100 µs. CICLO_100us must be cleared at initialization.
+	;CICLO_100us every 100 Âµs. CICLO_100us must be cleared at initialization.
 	LIST P= 16f876A
 	include "p16f876a.inc"
 
@@ -9,27 +9,27 @@
 	ORG	0
 	goto	inicio
 
-	ORG 4					;Definimos la dirección de la interrupción.
+	ORG 4					;Definimos la direcciÃ³n de la interrupciÃ³n.
 	goto	int_tmr
 	ORG	5
-	;Importante: No es lo mismo incrementar 1 vez cada ciclo que 1 vez cada 100us. Cuando tmr0 desborde será cuando sumaremos 1.
+	;Importante: No es lo mismo incrementar 1 vez cada ciclo que 1 vez cada 100us. Cuando tmr0 desborde serÃ¡ cuando sumaremos 1.
 inicio
 
-	BSF 	INTCON,GIE	;Habilitar la interrupción
+	BSF 	INTCON,GIE	;Habilitar la interrupciÃ³n
 	BSF	INTCON,T0IE	;Habilitar timer0
 
 	;Enable T0CS si queremos usar tiempo. (Bit del option register)
-	BSF STATUS,RP0		;Cambiamos al banco 1 poniendo RP0 a 1
+	BSF 	STATUS,RP0		;Cambiamos al banco 1 poniendo RP0 a 1
 	BCF	OPTION_REG,T0CS	;Ponemos T0CS a 0 para poner tmr0 en reloj interno(T0cs pin de option_register)
 	BSF	OPTION_REG,PSA	;Ponemos prescaler a 1 para que sea de watchdog timer
-	;Si queremos el rate1:1 PSA tendrá que ser del WDT(a 1), para otros rate tenemos que poner prescaler a 0 para que sea de timer0
+	;Si queremos el rate1:1 PSA tendrÃ¡ que ser del WDT(a 1), para otros rate tenemos que poner prescaler a 0 para que sea de timer0
 	BCF	STATUS,RP0		;Volvemos al banco 0
 	movlw	.156
 	movwf	TMR0	
-	clrf ciclo_100us	;Inicializamos la variable ciclo100us (Poniendo el registro a 0)
+	clrf 	ciclo_100us	;Inicializamos la variable ciclo100us (Poniendo el registro a 0)
 main 
-	;256-100=156 TMR0 tiene q empezar en 156
-	goto main
+	;main loop
+	goto 	main
 
 ;incf para sumar
 int_tmr
